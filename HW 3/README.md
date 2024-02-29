317fdd07c2ea709867a2e40e7b4079450337533a
lcmiles <lcmiles@uab.edu>
2024-02-29 08:08:10 -0600

    pushing HW3

---
+++ HW 3/README.md
@@ -0,0 +1,111 @@
+# Compilation Instructions
+
+```bash
+gcc -o <executable name> <filename> # this will compile the filename and return an executable with the executable name
+
+gcc -o lcmiles_HW03 lcmiles_HW03.c # this will compile lcmiles_HW03.c and output an executable called lcmiles_HW03
+```
+
+# Execution Instructions
+
+```bash
+./lcmiles_HW03 # this will execute lcmiles_HW03 and print all files in the directory
+
+./lcmiles_HW03 -v # this will execute lcmiles_HW03 and print all files in the directory with detailed information
+
+./lcmiles_HW03 -L <size limit> # this will execute lcmiles_HW03 and print all files in the directory equal to or less than the size limit in bytes
+
+./lcmiles_HW03 -s <string pattern> <search depth> # this will execute lcmiles_HW03 and print all files in the directory that contain the string pattern up to the search depth
+
+./lcmiles_HW03 -e <linux command string> # this will execute lcmiles_HW03 and call the given linux command on the file or directory
+
+./lcmiles_HW03 -v -s <string pattern> <search depth> -L <size limit> #arguments can also be combined like so
+
+OR
+
+make run # this will execute lcmiles_HW03 and print all files in the directory
+
+make search s=<string pattern> d=<depth> # this will execute lcmiles_HW03 and print all files in the directory that contain the string pattern up to the search depth
+
+make verbose # this will execute lcmiles_HW03 and print all files in the directory with detailed information
+
+make size limit L=<size limit> # this will execute lcmiles_HW03 and print all files in the directory equal to or less than the size limit in bytes
+```
+
+# My Files
+
+## HW1.c
+
+### main() [^1] [^2]
+
+* Description: 
+* Parameters:
+    * int argc - The number of command-line arguments
+    * char *argv[] - The character array containing the arguments used by getopt()
+* Returns: 0
+
+### traverseDirectory() [^5] [^6] [^7] [^8]
+
+* Description: Recursively traverses the file hierarchy, printing the correct information based on command-line arguments
+* Parameters:
+    * char *base_path - The starting path of the traversal
+    * const int root - Constant int used as a counter for traversal recursion
+    * const int verbose - Verbose argument flag applied when -v is used
+    * const int file_size_limit - Size limit given in command-line arguments when -L is used
+    * const char *linux_command - The command to be called on the given file or directory
+* Returns: void
+
+### traverseDirectoryWithSearch() [^3] [^5] [^6] [^7] [^8]
+
+* Description: Recursively traverses the file hierarchy up to the given search depth, printing any files that match the search_pattern substring, as well as the correct information based on command-line arguments
+* Parameters:
+    * char *base_path - The starting path of the traversal
+    * const int root - Constant int used as a counter for traversal recursion
+    * const int verbose - Verbose argument flag applied when -v is used
+    * const int file_size_limit - Size limit given in command-line arguments when -L is used
+    * const char *search_pattern - Substring that is searched for throughout the file hiearchy
+    * const int search_depth - The directory depth that will be traversed to when searching for files
+    * const char *linux_command - The command to be called on the given file or directory
+* Returns: void
+
+### printFileInfo() [^4]
+
+* Description: Prints file details based on the file status structure when the -v argument is applied
+* Parameters:
+    * const struct stat *file_stat - The file status structure passed from traverseDirectory() or traverseDirectoryWithSearch()
+    * const char  *file_name - The name of the given file
+* Returns: void
+
+# Code/Output screenshots
+
+![alt text]()
+
+![alt text]()
+
+![alt text]()
+
+![alt text]()
+
+![alt text]()
+
+# Link to GitHub reopository:
+
+
+
+# References
+
+[^1]: https://www.tutorialspoint.com/how-to-check-if-a-c-cplusplus-string-is-an-int#:~:text=Using%20built%2Din%20method%20isdigit,string%20does%20not%20contain%20int. (Used in main() for error handling of command-line arguments)
+
+[^2]: https://pubs.opengroup.org/onlinepubs/009696799/functions/getopt.html (Used in main to derive command-line arguments)
+
+[^3]: https://stackoverflow.com/questions/12784766/check-substring-exists-in-a-string-in-c (Used in traverseDirectoryWithSearch() to compare command-line arguments and file names)
+
+[^4]: https://www.geeksforgeeks.org/ctime-function-in-c-c/ (Used in printFileInfo() to create the string containing date and time of file access)
+
+[^5]: https://codeforwin.org/c-programming/c-program-to-list-all-files-in-a-directory-recursively (Used in both traverseDirectory() and traverseDirectoryWithSearch() to traverse the file directory)
+
+[^6]: https://stackoverflow.com/questions/31132699/directory-traversing-in-c (Used in both traverseDirectory() and traverseDirectoryWithSearch() to traverse the file directory)
+
+[^7]: https://stackoverflow.com/questions/3554120/open-directory-using-c (Used in both traverseDirectory() and traverseDirectoryWithSearch() to traverse the file directory)
+
+[^8]: https://www.tutorialspoint.com/c_standard_library/c_function_system.htm (Used in both traverseDirectory() and traverseDirectoryWithSearch() to call linux functions)
---
+++ HW 3/lcmiles_HW03.c
@@ -0,0 +1,227 @@
+/*
+Name: Logan Miles
+BlazerId: lcmiles
+Project #: 2
+To compile: 
+gcc -Wall -g lcmiles_HW02.c -o lcmiles_HW02 -lm -fno-pie -no-pie 
+OR
+make
+To run:
+./lcmiles_HW02 
+./lcmiles_HW02 -v 
+./lcmiles_HW02 -L <size limit>
+./lcmiles_HW02 -s <string pattern> <search depth>
+./lcmiles_HW02 -e <linux command string>
+OR
+make run
+make search s=<string pattern> d=<depth>
+make verbose
+make size limit L=<size limit>
+make linux cmd e=<linux command string>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <bits/getopt_core.h>
+#include <ctype.h>
+
+//function prototypes
+int traverseDirectory(char *base_path, const int root, const int verbose, const int file_size_limit, const char *linux_command);
+int traverseDirectoryWithSearch(char *base_path, const int root, const int verbose, const int file_size_limit, const char *search_pattern, const int search_depth, const char *linux_command);
+void printFileInfo(const struct stat *file_stat, char *file_name);
+
+int main(int argc, char *argv[]) {
+    int verbose = 0;
+    int file_size_limit = -1;
+    char *search_pattern = NULL;
+    int search_depth = -1;
+    char *path;
+    char *ptr;
+    char *linux_command = NULL;
+
+
+    int opt;
+    while ((opt = getopt(argc, argv, "vL:s:e:")) != -1) { //loop through command line options
+        switch (opt)
+        {
+        case 'v': //verbose case
+            verbose = 1; //set verbose flag
+            break;
+        case 'L': //size limit case
+            ptr = optarg; //set pointer to argument
+            while (*ptr) { //loop through argument characters
+                if (!(isdigit(*ptr))) { //if the character is not a digit then return error code
+                    fprintf(stderr, "Usage: %s [-v] [-L <file size (positive int in bytes)>] [-s <string pattern> <depth (positive int in directory depth)>] [directory]\n", argv[0]);
+                    return -1;
+                }
+                ptr++;
+            }
+            file_size_limit = atoi(optarg); //convert to int and assign variable
+            if (file_size_limit <= 0) { //if the file size limit is negative return error code
+                fprintf(stderr, "Usage: %s [-v] [-L <file size (positive int in bytes)>] [-s <string pattern> <depth (positive int in directory depth)>] [directory]\n", argv[0]);
+                return -1;
+            }
+            break;
+        case 's': //search case
+            search_pattern = optarg; //assign the argument to search pattern character array
+            if (argv[optind] != NULL) { //if the search depth argument available assign it to variable
+                search_depth = atoi(argv[optind]);
+            }
+            else { //else return error code
+                fprintf(stderr, "Usage: %s [-v] [-L <file size (positive int in bytes)>] [-s <string pattern> <depth (positive int in directory depth)>] [directory]\n", argv[0]);
+                return -1;
+            }
+            optind++;  
+            break;
+        case 'e': //run linux command case
+            linux_command = optarg;
+            break;
+        default: //return error code by default
+            fprintf(stderr, "Usage: %s [-v] [-L <file size (positive int in bytes)>] [-s <string pattern> <depth (positive int in directory depth)>] [directory]\n", argv[0]);
+            return -1;
+        }
+    }
+
+    if (optind < argc) { //if there is another argument after the switch case then set it to the path
+        path = argv[optind];
+    } 
+    else { //otherwise set the default path to the working directory
+        path = ".";
+    }
+
+    if (search_pattern == NULL && search_depth == -1) { //if the search_pattern and search_depth variables are set to their default values, execute traverseDirectory()
+        traverseDirectory(path, 0, verbose, file_size_limit,linux_command);
+    }
+    else traverseDirectoryWithSearch(path, 0, verbose, file_size_limit, search_pattern, search_depth,linux_command); //otherwise execute traverseDirectoryWithSearch()
+
+    return 0;
+}
+
+int traverseDirectory(char *base_path, const int root, const int verbose, const int file_size_limit, const char *linux_command) {
+    
+    int i;
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(base_path);
+    
+
+    if (!dir) { //if the directory is not successfully opened then return error code
+        perror("opendir");
+        return -1;
+    }
+
+    while ((dp = readdir(dir)) != NULL) { //loop through directory entries
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) { //exclude the "." and ".." directories
+
+            strcpy(path, base_path); //copy the base path to the path
+
+            strcat(path, "/"); //append "/" to the path
+            strcat(path, dp->d_name); //append directory name to the path
+
+            struct stat file_stat; //get the file status and store it in a structure
+            if (lstat(path, &file_stat) < 0) { //return error code if the file cannot be accessed
+                perror("lstat");
+                return -1;
+            }
+
+            if (file_size_limit != -1 && S_ISREG(file_stat.st_mode) && file_stat.st_size < file_size_limit) continue; //skip files above the size limit if the flag is set to anything but default
+
+            char *file_name = dp->d_name; //set the file name
+
+            for (i = 0; i < root; i++) { //print indentation per file based on the root level
+                printf("\t");
+            }
+            
+            if (verbose) printFileInfo(&file_stat, file_name); //print the file info if verbose flag set to true
+
+            if (verbose == 0) printf("%s\n", dp->d_name); //print the file name if the verbose flag set to false
+
+            if (linux_command != NULL) {
+
+                char command[10000];
+
+                    snprintf(command, sizeof(command), "%s %s", linux_command, path);
+                    
+                    system(command);
+            }
+
+            if (S_ISDIR(file_stat.st_mode)) { //if the file is a directory recursively call traverseDirectory()
+                traverseDirectory(path, root + 1, verbose, file_size_limit, linux_command);
+            }
+        }
+    }
+    closedir(dir);
+    return 0;
+}
+
+int traverseDirectoryWithSearch(char *base_path, const int root, const int verbose, const int file_size_limit, const char *search_pattern, const int search_depth, const char *linux_command) { //code is similar to traverseDirectory() except for file printing exclusion
+    char path[1000];
+    struct dirent *dp;
+    DIR *dir = opendir(base_path);
+
+    if (!dir) {
+        perror("opendir");
+        return -1;
+    }
+
+    while ((dp = readdir(dir)) != NULL) {
+        if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0) {
+
+            strcpy(path, base_path);
+            strcat(path, "/");
+            strcat(path, dp->d_name);
+
+            struct stat file_stat;
+            if (lstat(path, &file_stat) < 0) {
+                perror("lstat");
+                return -1;
+            }
+
+            if (file_size_limit != -1 && S_ISREG(file_stat.st_mode) && file_stat.st_size < file_size_limit)
+                continue;
+
+            char *file_name = dp->d_name;
+
+            if (root <= search_depth) { //if the root is less than or equal to the search depth 
+
+                if (strstr(file_name, search_pattern) != NULL) { //if the file name contains the search pattern
+                    
+                    if (verbose) printFileInfo(&file_stat, file_name);
+
+                    if (verbose == 0) printf("%s\n", dp->d_name);
+
+                    if (linux_command != NULL) {
+
+                    char command[10000];
+
+                    snprintf(command, sizeof(command), "%s %s", linux_command, path);
+
+                    system(command);
+            }
+                }
+            }
+
+            if (S_ISDIR(file_stat.st_mode)) {
+                traverseDirectoryWithSearch(path, root + 1, verbose, file_size_limit, search_pattern, search_depth, linux_command);
+            }
+        }
+    }
+    closedir(dir);
+    return 0;
+}
+
+void printFileInfo(const struct stat *file_stat, char *file_name) {
+    char *time_string = ctime(&file_stat->st_atime); //create string containing date and time file was last accessed
+    time_string[strcspn(time_string, "\n")] = '\0'; //remove newline character from time string
+    if (S_ISDIR(file_stat->st_mode)) { //if the file is a directory, always print the file size as 0
+        printf("%s (Size: 0, Permissions: %o, Last Access: %s)\n", file_name, (file_stat->st_mode & 0777), time_string); //print file information from stat structure
+    } 
+    else {
+        printf("%s (Size: %lld, Permissions: %o, Last Access: %s)\n", file_name, (long long)file_stat->st_size, (file_stat->st_mode & 0777), time_string); //print file information from stat structure
+    }
+}
